e94bea7646d35c2f2c8be0f0591bf7cc9fa1f8f8
zhendongwang <zhw65@pitt.edu>
2018-04-06 09:08:05 -0400

    update version

--- dist/test/allTest.js
+++ dist/test/allTest.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var lib_1 = require("../lib");
+var stringBatchUpdate_1 = require("../lib/stringBatchUpdate");
 it('test replace', function () {
-    var newText = lib_1.strintBatchReplace("abc", [
+    var newText = stringBatchUpdate_1.stringBatchUpdate("abc", [
         {
-            type: lib_1.EDIT_TYPE.replace,
+            type: stringBatchUpdate_1.EDIT_TYPE.replace,
             start: 1,
             end: 3,
             text: "yy"
@@ -13,45 +13,80 @@ it('test replace', function () {
     expect(newText).toEqual("ayy");
 });
 it('test insert', function () {
-    var newText = lib_1.strintBatchReplace("abc", [
+    var newText = stringBatchUpdate_1.stringBatchUpdate("abc", [
         {
-            type: lib_1.EDIT_TYPE.insert,
+            type: stringBatchUpdate_1.EDIT_TYPE.insert,
             start: 1,
             text: "x"
         }
     ]);
     expect(newText).toEqual("axbc");
 });
-it('test together', function () {
-    var newText = lib_1.strintBatchReplace("abc", [
+it('test delete', function () {
+    var newText = stringBatchUpdate_1.stringBatchUpdate("abc", [
         {
-            type: lib_1.EDIT_TYPE.insert,
+            type: stringBatchUpdate_1.EDIT_TYPE.delete,
+            start: 1,
+            end: 2
+        }
+    ]);
+    expect(newText).toEqual("ac");
+});
+it('test delete (delete is longer than next insert)', function () {
+    var newText = stringBatchUpdate_1.stringBatchUpdate("import 'a'\n", [
+        {
+            type: stringBatchUpdate_1.EDIT_TYPE.delete,
+            start: 0,
+            end: 12
+        },
+        {
+            type: stringBatchUpdate_1.EDIT_TYPE.insert,
+            start: 11,
+            text: "import 'b'"
+        }
+    ]);
+    expect(newText).toEqual("import 'b'");
+});
+it('test together', function () {
+    var newText = stringBatchUpdate_1.stringBatchUpdate("abc", [
+        {
+            type: stringBatchUpdate_1.EDIT_TYPE.insert,
             start: 1,
             text: "x"
         },
         {
-            type: lib_1.EDIT_TYPE.replace,
+            type: stringBatchUpdate_1.EDIT_TYPE.delete,
+            start: 1,
+            end: 2
+        },
+        {
+            type: stringBatchUpdate_1.EDIT_TYPE.replace,
             start: 2,
             end: 3,
             text: "yy"
         }
     ]);
-    expect(newText).toEqual("axbyy");
+    expect(newText).toEqual("axyy");
 });
 it('test reorder together', function () {
-    var newText = lib_1.strintBatchReplace("abc", [
+    var newText = stringBatchUpdate_1.stringBatchUpdate("abc", [
         {
-            type: lib_1.EDIT_TYPE.replace,
+            type: stringBatchUpdate_1.EDIT_TYPE.replace,
             start: 2,
             end: 3,
             text: "yy"
         },
         {
-            type: lib_1.EDIT_TYPE.insert,
+            type: stringBatchUpdate_1.EDIT_TYPE.insert,
             start: 1,
             text: "x"
+        },
+        {
+            type: stringBatchUpdate_1.EDIT_TYPE.delete,
+            start: 1,
+            end: 2
         }
     ]);
-    expect(newText).toEqual("axbyy");
+    expect(newText).toEqual("axyy");
 });
 //# sourceMappingURL=allTest.js.map
